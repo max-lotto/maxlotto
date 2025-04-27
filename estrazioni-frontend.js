12eecc86bcfcdae7e7d276fb4b5457d6fb5ebe4d
max-lotto <ascroma31@gmail.com>
2025-04-27 15:58:12 +0200

    Update estrazioni-frontend.js

--- estrazioni-frontend.js
+++ estrazioni-frontend.js
@@ -9,26 +9,28 @@ async function caricaUltimaEstrazione() {
     data.innerText = `Data Estrazione: ${estrazione.data}`;
 
     let html = `
-      <tr>
-        <th>Ruota</th>
-        <th>Numeri</th>
-      </tr>
+      <table style="width:100%; border-collapse: collapse;">
+        <tr>
+          <th style="background-color: #003049; color: white; padding: 10px;">Ruota</th>
+          <th style="background-color: #003049; color: white; padding: 10px;">Numeri Estratti</th>
+        </tr>
     `;
 
     for (const ruota in estrazione.ruote) {
       html += `
         <tr>
-          <td>${ruota}</td>
-          <td>${estrazione.ruote[ruota].join(', ')}</td>
+          <td style="border: 1px solid #ccc; padding: 10px;">${ruota}</td>
+          <td style="border: 1px solid #ccc; padding: 10px;">${estrazione.ruote[ruota].join(' - ')}</td>
         </tr>
       `;
     }
 
+    html += `</table>`;
     tabella.innerHTML = html;
   } catch (err) {
     console.error('Errore caricamento estrazione:', err);
   }
 }
 
-// Carica appena apre pagina
 caricaUltimaEstrazione();
+
